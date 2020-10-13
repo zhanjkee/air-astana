b52ec160128a7508ab3e109ba7fde0521112d175
zhanjkee <zhanjkee@gmail.com>
2020-10-13 13:15:38 +0300

    [auth-service] [add] first commit.

---
+++ AirAstana/Services/Auth/AirAstana.Auth.Api/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirAstana.Auth.Api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ToDo()
+        {
+            return Ok("ToDo");
+        }
+    }
+}
