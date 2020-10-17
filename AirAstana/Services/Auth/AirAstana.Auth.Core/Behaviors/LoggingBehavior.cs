90ec0f0fde32c4ba5edc11edc5e2ab751523edc0
zhanjkee <zhanjkee@gmail.com>
2020-10-18 00:10:26 +0300

    [auth-service] [add] logging, validation behaviors.

---
+++ AirAstana/Services/Auth/AirAstana.Auth.Core/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using AirAstana.Shared.Extensions;
+
+namespace AirAstana.Auth.Core.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger) => _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _logger.LogInformation("----- Handling request {RequestName} ({@RequestParams})", request.GetGenericTypeName(), request);
+            var response = await next();
+            _logger.LogInformation("----- Request {RequestName} handled - response: {@Response}", request.GetGenericTypeName(), response);
+
+            return response;
+        }
+    }
+}
---
+++ AirAstana/Services/Auth/AirAstana.Auth.Core/Behaviors/ValidatorBehavior.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AirAstana.Shared.Extensions;
+
+namespace AirAstana.Auth.Core.Behaviors
+{
+    public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<ValidatorBehavior<TRequest, TResponse>> _logger;
+        private readonly IValidator<TRequest>[] _validators;
+
+        public ValidatorBehavior(IValidator<TRequest>[] validators, ILogger<ValidatorBehavior<TRequest, TResponse>> logger)
+        {
+            _validators = validators;
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var typeName = request.GetGenericTypeName();
+
+            _logger.LogInformation("----- Validating request {RequestType}", typeName);
+
+            var failures = _validators
+                .Select(v => v.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                _logger.LogWarning("Validation errors - {RequestType} - Request: {@Request} - Errors: {@ValidationErrors}", typeName, request, failures);
+
+                throw new Exception($"Request Validation Errors for type {typeof(TRequest).Name}", new ValidationException("Validation exception", failures));
+            }
+
+            return await next();
+        }
+    }
+}
