96de430e4a334a26949de4afa84d4e64b0d7b5ed
zhanjkee <zhanjkee@gmail.com>
2020-10-18 01:32:46 +0300

    [auth-service] [add] register user handler.

---
+++ AirAstana/Services/Auth/AirAstana.Auth.Core/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AirAstana.Auth.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AirAstana.Auth.Core.Commands.RegisterUser
+{
+    public sealed class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserResponse>
+    {
+        private readonly UserManager<UserEntity> _userManager;
+
+        public RegisterUserCommandHandler(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public async Task<RegisterUserResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var userEntity = new UserEntity(request.FirstName, request.LastName, request.UserName, request.Email);
+            var result = await _userManager.CreateAsync(userEntity, request.Password);
+            return result.Succeeded ? new RegisterUserResponse(userEntity.Id, true) : new RegisterUserResponse(result.Errors.Select(e => e.Description));
+        }
+    }
+}
---
+++ AirAstana/Services/Auth/AirAstana.Auth.Core/Commands/RegisterUser/RegisterUserValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace AirAstana.Auth.Core.Commands.RegisterUser
+{
+    public sealed class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.");
+        }
+    }
+}
