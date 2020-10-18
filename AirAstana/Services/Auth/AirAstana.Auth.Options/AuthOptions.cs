36b8472b421eeb38b31cca9bf16c8bedd2d2f3c0
zhanjkee <zhanjkee@gmail.com>
2020-10-18 15:15:52 +0300

    [auth-service] [add] auth options.

---
+++ AirAstana/Services/Auth/AirAstana.Auth.Options/AuthOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace AirAstana.Auth.Options
+{
+    public class AuthOptions
+    {
+        /// <summary>
+        ///     Gets or sets the secret key.
+        /// </summary>
+        public string SecretKey { get; set; }
+        /// <summary>
+        ///     Gets or sets the connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
