0d25bf06cc27aecceed7361011ecee25c4182839
zhanjkee <zhanjkee@gmail.com>
2020-10-20 14:17:37 +0300

    [flights-service] [add] mssql support.

---
+++ AirAstana/Services/Flights/AirAstana.Flights.Data.SqlServer/SqlServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AirAstana.Flights.Data.Context;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirAstana.Flights.Data.SqlServer
+{
+    public class SqlServerDbContext : FlightsContext
+    {
+        public SqlServerDbContext([NotNull] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ AirAstana/Services/Flights/AirAstana.Flights.Data.SqlServer/SqlServerDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AirAstana.Flights.Data.Abstract;
+using AirAstana.Flights.Data.Context;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirAstana.Flights.Data.SqlServer
+{
+    public sealed class SqlServerDbContextFactory : IDbContextFactory
+    {
+        private readonly string _connectionString;
+        public SqlServerDbContextFactory([NotNull]string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString)) throw new ArgumentNullException(nameof(connectionString));
+            _connectionString = connectionString;
+        }
+
+        public FlightsContext Create()
+        {
+            if (string.IsNullOrEmpty(_connectionString)) throw new Exception($"Connection string is null or empty.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<SqlServerDbContext>()
+                .UseSqlServer(_connectionString);
+
+            var context = new SqlServerDbContext(optionsBuilder.Options);
+            context.Database.Migrate();
+            return context;
+        }
+    }
+}
