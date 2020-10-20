96548b54b5e33f62da4d63ba11187d5e2a25af99
zhanjkee <zhanjkee@gmail.com>
2020-10-20 19:49:19 +0300

    [flights-servicec] [add] data module, options.

---
+++ AirAstana/Services/Flights/AirAstana.Flights.Data.Autofac/DataModule.cs
@@ -0,0 +1,32 @@
+﻿using AirAstana.Flights.Core.Interfaces.Repositories;
+using AirAstana.Flights.Data.Abstract;
+using AirAstana.Flights.Data.Context;
+using AirAstana.Flights.Data.Repositories;
+using AirAstana.Flights.Data.SqlServer;
+using AirAstana.Flights.Options;
+using Autofac;
+
+namespace AirAstana.Flights.Data.Autofac
+{
+    public class DataModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(x =>
+            {
+                var configuration = x.Resolve<FlightsServiceOptions>();
+                return new SqlServerDbContextFactory(configuration.ConnectionString);
+            }).As<IDbContextFactory>().InstancePerLifetimeScope();
+
+            builder.Register(x =>
+            {
+                var configuration = x.Resolve<FlightsServiceOptions>();
+                var dbContextFactory = new SqlServerDbContextFactory(configuration.ConnectionString);
+                return dbContextFactory.Create();
+            }).As<FlightsContext>().InstancePerLifetimeScope();
+
+            builder.RegisterType<FlightRepository>().As<IFlightRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<LocationRepository>().As<ILocationRepository>().InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ AirAstana/Services/Flights/AirAstana.Flights.Options/FlightsServiceOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace AirAstana.Flights.Options
+{
+    public class FlightsServiceOptions
+    {
+        /// <summary>
+        ///     The section name.
+        /// </summary>
+        public const string SectionName = "Flights";
+
+        /// <summary>
+        ///     Gets or sets the connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
