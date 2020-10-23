cc352d2c95a4dd1eac4cfe73a6963259186c3135
zhanjkee <zhanjkee@gmail.com>
2020-10-23 15:26:56 +0300

    [auth-service] [add] auth constants.

---
+++ AirAstana/Services/Auth/AirAstana.Auth.Constants/AuthConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace AirAstana.Auth.Constants
+{
+    public static class AuthConstants
+    {
+        public static class Roles
+        {
+            public const string Administrator = nameof(Administrator),
+                                Moderator = nameof(Moderator);
+        }
+    }
+}
